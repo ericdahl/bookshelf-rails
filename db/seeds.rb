9eb27dc870d66f26e8ba81002db279ba9d34adb5
Eric Dahl <eric.c.dahl@gmail.com>
2025-05-17 12:01:26 -0700

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,58 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+puts "Seeding database..."
+
+# Series
+stormlight = Series.find_or_create_by!(name: "Stormlight Archive") do |s|
+  s.description = "Epic fantasy series by Brandon Sanderson."
+end
+
+dune_series = Series.find_or_create_by!(name: "Dune Saga") do |s|
+  s.description = "Science fiction series by Frank Herbert."
+end
+
+# Books
+Book.find_or_create_by!(title: "The Way of Kings") do |b|
+  b.author = "Brandon Sanderson"
+  b.series = stormlight
+  b.status = "read"
+  b.rating = 10
+  b.book_type = "ebook"
+  b.publication_year = 2010
+  b.page_count = 1007
+  b.date_added = Time.current
+  b.date_finished = Time.current - 1.month
+  b.comments = "Amazing start to the series."
+end
+
+Book.find_or_create_by!(title: "Words of Radiance") do |b|
+  b.author = "Brandon Sanderson"
+  b.series = stormlight
+  b.status = "want_to_read"
+  b.book_type = "ebook"
+  b.publication_year = 2014
+  b.page_count = 1087
+  b.date_added = Time.current
+end
+
+Book.find_or_create_by!(title: "Dune") do |b|
+  b.author = "Frank Herbert"
+  b.series = dune_series
+  b.status = "read"
+  b.rating = 9
+  b.book_type = "physical_book"
+  b.publication_year = 1965
+  b.page_count = 412
+  b.date_added = Time.current
+end
+
+Book.find_or_create_by!(title: "Project Hail Mary") do |b|
+  b.author = "Andy Weir"
+  b.status = "currently_reading"
+  b.book_type = "audiobook"
+  b.publication_year = 2021
+  b.page_count = 496 # Or duration for audiobook
+  b.date_added = Time.current
+  b.date_started = Time.current - 1.week
+end
+
+puts "Seeding complete!"
