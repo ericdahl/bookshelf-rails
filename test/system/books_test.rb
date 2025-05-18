450f2930e11f945fb119c4f877b283b210114a02
Eric Dahl <eric.c.dahl@gmail.com>
2025-05-18 12:26:06 -0700

    fix functional tests

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -6,14 +6,19 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
-    visit books_url
+    visit books_path
     assert_selector "h1", text: "Books"
   end
 
   test "should create book" do
-    visit books_url
+    visit books_path
     click_on "New book"
 
+    fill_in "Title", with: @book.title
+    fill_in "Author", with: @book.author
+    fill_in "Isbn 13", with: "999999999999#{Time.current.to_i}"
+    select @book.status.humanize, from: "Status"
+    select @book.book_type.humanize, from: "Book type"
     click_on "Create Book"
 
     assert_text "Book was successfully created"
@@ -21,9 +26,14 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "should update Book" do
-    visit book_url(@book)
+    visit book_path(@book)
     click_on "Edit this book", match: :first
 
+    fill_in "Title", with: @book.title
+    fill_in "Author", with: @book.author
+    fill_in "Isbn 13", with: @book.isbn_13
+    select @book.status.humanize, from: "Status"
+    select @book.book_type.humanize, from: "Book type"
     click_on "Update Book"
 
     assert_text "Book was successfully updated"
@@ -31,7 +41,7 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "should destroy Book" do
-    visit book_url(@book)
+    visit book_path(@book)
     accept_confirm { click_on "Destroy this book", match: :first }
 
     assert_text "Book was successfully destroyed"
--- test/system/series_test.rb
+++ test/system/series_test.rb
@@ -6,16 +6,16 @@ class SeriesTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
-    visit series_url
+    visit series_index_path
     assert_selector "h1", text: "Series"
   end
 
   test "should create series" do
-    visit series_url
+    visit series_index_path
     click_on "New series"
 
-    fill_in "Description", with: @series.description
-    fill_in "Name", with: @series.name
+    fill_in "Description", with: "A new test series"
+    fill_in "Name", with: "New Test Series #{Time.current.to_i}"
     click_on "Create Series"
 
     assert_text "Series was successfully created"
@@ -23,11 +23,11 @@ class SeriesTest < ApplicationSystemTestCase
   end
 
   test "should update Series" do
-    visit series_url(@series)
+    visit series_path(@series)
     click_on "Edit this series", match: :first
 
     fill_in "Description", with: @series.description
-    fill_in "Name", with: @series.name
+    fill_in "Name", with: "Updated Series #{Time.current.to_i}"
     click_on "Update Series"
 
     assert_text "Series was successfully updated"
@@ -35,7 +35,7 @@ class SeriesTest < ApplicationSystemTestCase
   end
 
   test "should destroy Series" do
-    visit series_url(@series)
+    visit series_path(@series)
     accept_confirm { click_on "Destroy this series", match: :first }
 
     assert_text "Series was successfully destroyed"
