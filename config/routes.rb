e009866c818a22307f1d4a60ed18deb6d8256382
Eric Dahl <eric.c.dahl@gmail.com>
2025-05-25 10:37:15 -0700

    remove old index

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "books#index"
 
   namespace :api do
     namespace :v1 do
